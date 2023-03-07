3719539877a87bdf24035b4e064bf3f440a7d021
Nastasia <nastasiahyam@gmail.com>
2023-03-06 22:49:11 -0500

    Basic skeleton & test API

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { note, interval } from "@tonaljs/tonal";
+import { chord }    from "@tonaljs/chord";
+
+//console.log(note("Ab4"));
+
+//const myInterval = interval('5P');
+
+//console.log(myInterval);
+
+console.log(chord('Ebmaj7'));
